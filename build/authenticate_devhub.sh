d42bf2ec1c23f59119a310ec2e06a43d0574dfe4
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-06-12 18:38:16 +0530

    Updating authentication script

--- build/authenticate_devhub.sh
+++ build/authenticate_devhub.sh
@@ -1,1 +1,9 @@
-node_modules/sfdx-cli/bin/run force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile server.key --username $USERNAME --setdefaultdevhubusername -a HubOrg
+echo "Reading private key..."
+echo $SERVER_KEY | base64 -d > server.key
+if [ -z $CONSUMERKEY ]
+then
+	echo "Client Id not found..."
+	exit 1
+else
+	node_modules/sfdx-cli/bin/run force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile server.key --username $USERNAME --setdefaultdevhubusername -a HubOrg	
+fi
--- build/install_sfdx.sh
+++ build/install_sfdx.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-echo $SERVER_KEY | base64 -d > server.key
 export SFDX_USE_GENERIC_UNIX_KEYCHAIN=true
 export SFDX_DOMAIN_RETRY=300
 npm install sfdx-cli
