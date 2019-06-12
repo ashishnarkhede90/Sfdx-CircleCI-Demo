00bbc1dee55392b202c5ed8e77dc7e8d2fbf16c1
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-06-12 18:48:08 +0530

    Updating script

--- build/authenticate_devhub.sh
+++ build/authenticate_devhub.sh
@@ -1,9 +1,30 @@
-echo "Reading private key..."
-echo $SERVER_KEY | base64 -d > server.key
-if [ -z $CONSUMERKEY ]
+echo "1. Reading private key..."
+if [ -z $SERVER_KEY]
 then
-	echo "Client Id not found..."
+	echo "Private key missing..."
 	exit 1
 else
+	echo "Private key... OK"		
+	echo $SERVER_KEY | base64 -d > server.key
+fi
+
+echo "2. Reading username"
+if [ -z $USERNAME ]
+then
+	echo "Username missing..."
+	exit 1
+else
+	echo "Username... OK"
+fi
+
+echo "3. Reading client Id"
+if [ -z $CONSUMERKEY ]
+then
+	echo "Client Id missing..."
+	exit 1
+else
+	echo "Client Id... OK"
 	node_modules/sfdx-cli/bin/run force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile server.key --username $USERNAME --setdefaultdevhubusername -a HubOrg	
 fi
+
+echo "4. Authenticating to Dev Hub"
